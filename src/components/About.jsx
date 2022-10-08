0f877a7f0b693d5eba72a264ce6d14124ffb6929
NokibulAlam <nokibul.alam02@gmail.com>
2022-10-09 02:27:41 +0600

    SKILLS page works going on

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,27 +5,30 @@ const About = () => {
   return (
     <div name='about' className='w-full h-screen bg-gradient-to-b from-[#090809] to-gray-800 text-gray-300'>
       <div className='flex flex-col justify-center items-center w-full h-full'>
+        
         <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
           <div className='sm:text-right pb-8 pl-4' data-aos="fade-down" data-aos-duration="1000">
             <p className='text-4xl font-bold inline border-b-4 border-gray-600 rounded'>
               About
             </p>
           </div>
-          <div></div>
+
         </div>
+
         <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4 items-center'>
           <div className='font-bold pt-5' data-aos="fade-right" data-aos-duration="1000">
             <img src={Nokib} alt="Nokib.png" className='mx-auto'/>
           </div>
           <div data-aos="fade-left" data-aos-duration="1000">
             <p className='text-3xl py-4'>Hi. I'm Nokib. Well-organised, independent and a family oriented happy person.</p>
-            <p>I'm a full stack web developer. I design and code beautifully simple things, and I love what I do. <br />
+            <p className='text-white'>I'm a full stack web developer. I design and code beautifully simple things, and I love what I do. <br />
               I'm also passionate about Street Photography and won some prizes from exhibition. 
               I have a wedding photography team named “Dream Studio”. We have been working together for 6 years.
              <br/> I’m always curious to learn more when it comes to new technologies and creative coding.
             </p>
           </div>
         </div>
+
       </div>
     </div>
   )
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,73 @@
-import React from 'react';
+import React from 'react'; //data-aos="fade-down" data-aos-duration="1000"
 
 const Skills = () => {
+
+  const languages = ([
+    {
+      name: "C/C++",
+      progress: "80%",
+      width: "80%"
+    },
+    {
+      name: "JavaScript",
+      progress: "85%",
+      width: "85%"
+    },
+    {
+      name: "HTML",
+      progress: "90%",
+      width: "90%"
+    },
+    {
+      name: "CSS",
+      progress: "75%",
+      width: "75%"
+    },
+    {
+      name: "Bootstrap",
+      progress: "70%",
+      width: "70%"
+    },
+    {
+      name: "TwilWind CSS",
+      progress: "55%",
+      width: "55%"
+    },
+    {
+      name: "C",
+      progress: "80%",
+      width: "80%"
+    },
+  ]);
+
   return (
     <div name="skills" className='w-full h-screen bg-theme'>
-        <h1 className='text-4xl text-txtT'>This is a Skills sections</h1>
+      <div className='flex flex-col justify-center items-center w-full h-full'>
+
+        <div className='text-center pb-8 pl-4'>
+          <p className='text-4xl text-txtT font-bold inline border-b-4 border-gray-600 rounded'>
+            Skills
+          </p>
+        </div>
+
+        <div className='max-w-[1000px] w-full h-full grid sm:grid-cols-2 gap-8 px-4 items-start'>
+          <div>
+            {languages.map((lan, i) => (
+              <div key={i}> 
+                <div className="flex justify-between mb-1">
+                  <span className="text-base font-medium text-blue-700 dark:text-white">{lan.name}</span>
+                  <span className="text-sm font-medium text-blue-700 dark:text-white">{lan.progress}</span>
+                </div>
+                <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                  <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: "45%" }}></div>
+                </div>
+              </div>
+            ))}
+
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
